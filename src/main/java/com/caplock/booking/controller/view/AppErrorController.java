cc9da57f5bae0c79c0f9958525e60da055f41cd3
Robert <robert32176@outlook.com>
2026-02-26 08:48:00 +0200

    small changes

---
+++ src/main/java/com/caplock/booking/controller/view/AppErrorController.java
@@ -0,0 +1,21 @@
+package com.caplock.booking.controller.view;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            model.addAttribute("status", status.toString());
+        }
+        return "ui/error/error";
+    }
+}
